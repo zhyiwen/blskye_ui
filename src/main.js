825c152a069233efae94b8c7e8ae0c310a267a55
zhyiwen <zhang92107@sina.com>
2019-04-03 16:29:33 +0800

    引用复选框美化插件

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 import "font-awesome/css/font-awesome.css";
-import '../src/scss/blskye.scss'
+import "../src/assets/css/awesome-bootstrap-checkbox.css";
+import "../src/assets/css/normalize.css";
+import "../src/scss/blskye.scss";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
