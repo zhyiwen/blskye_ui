4df6bc0371f1123dea91c79a2d175caa81d2081a
zhyiwen <zhang92107@sina.com>
2020-06-10 13:46:22 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,6 @@
         <router-view/>
       </div>
     </div>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/> -->
   </div>
 </template>
 
